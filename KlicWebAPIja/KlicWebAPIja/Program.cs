4c843b3d773080fbba24b121c09d492480943014
Barbara <barbara.pusnar@scng.si>
2019-04-15 09:35:29 +0200

    klic

---
+++ KlicWebAPIja/KlicWebAPIja/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlicWebAPIja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task RunAsync()
+        {
+            using (var klient = new HttpClient())
+            {
+                klient.BaseAddress = new Uri("http://localhost:24053/");
+                klient.DefaultRequestHeaders.Accept.Clear();
+                klient.DefaultRequestHeaders.Accept.Add(
+                    new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue
+                    ("application/json"));
+               HttpResponseMessage odg= await klient.GetAsync("api/Products/1");
+                if (odg.IsSuccessStatusCode)
+                {
+                    Product p = await odg.Content.ReadAsAsync<Product>();
+                    Console.WriteLine(p.Ime+" "+p.Cena);
+                }
+            }
+        }
+    }
+}
---
+++ MVCVaja/MVCVaja/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCVaja.MvcApplication" Language="C#" %>
